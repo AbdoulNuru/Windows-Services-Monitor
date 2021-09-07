5df6cbcd7b7e0130fc4d997442c2f4b8eb3d2730
AbdoulNuru <abdoulniyigena@gmail.com>
2021-09-07 11:08:51 +0200

    ft: add the script runner

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const exec = require('child_process').exec;
+
+const isRunning = (query, cb) => {
+    let platform = process.platform;
+    let cmd = '';
+    switch (platform) {
+        case 'win32' : cmd = `tasklist`; break;
+        case 'darwin' : cmd = `ps -ax | grep ${query}`; break;
+        case 'linux' : cmd = `ps -A`; break;
+        default: break;
+    }
+    exec(cmd, (err, stdout, stderr) => {
+        console.log(query.toLowerCase());
+        cb(stdout.toLowerCase().indexOf(query.toLowerCase()) > -1);
+    });
+}
+
+isRunning('chrome', (status) => {
+    console.log(status); // true|false
+})
